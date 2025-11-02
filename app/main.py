dbdb4d1170ae08d70150e93f1cb84eeff4ef840c
voduynghia <voduyanqn1972@gmail.com>
2025-11-02 13:15:36 +0700

    feat: add ci and cd workflows for FastAPI

--- app/main.py
+++ app/main.py
@@ -4,4 +4,10 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, This is FastAPI deployed on Azure Kubernetes Service (AKS)! version 2"}
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
